453168de82fed11620623cc13be3f995d79ce7b9
RicardoPayan <a218206581@unison.mx>
2022-06-23 11:03:41 -0700

    Alerts finished

--- 6- Weekly expensis/js/app.js
+++ 6- Weekly expensis/js/app.js
@@ -8,6 +8,7 @@ const gastoListado=document.querySelector('#gastos ul');
 eventListeners();
 function eventListeners(){
     document.addEventListener('DOMContentLoaded',preguntarPresupuesto);
+    formulario.addEventListener('submit',agregarGastos);
 }
 
 
@@ -20,6 +21,11 @@ class Presupuesto{
         this.gastos=[];
     }
 
+    nuevoGasto(gasto){
+        
+        this.gastos = [...this.gastos,gasto]; //Creamos una copia del gastos y le agregamos el nuevo gasto
+        
+    }
 
 }
 
@@ -30,6 +36,57 @@ class UI{
         //Agregar al HTML
         document.querySelector('#total').textContent=presupuesto
         document.querySelector('#restante').textContent=restante
+    }
+
+    imprimirAlerta(mensaje,tipo){
+        //Crear el div
+        const divAlerta=document.createElement('div');
+        divAlerta.classList.add('alert','text-center');
+        
+        if(tipo==='error'){
+            divAlerta.classList.add('alert-danger');
+        }else{
+            divAlerta.classList.add('alert-success');
+        }
+
+        divAlerta.textContent=mensaje;
+        
+        //Insertar en el HTML
+        document.querySelector('.primario').insertBefore(divAlerta,formulario);
+
+        //Quitar HTML
+        setTimeout(() => {
+            divAlerta.remove();
+        }, 3000);
+    }
+
+    agregarGastoListado(gastos){
+        limpiarHTML();
+        gastos.forEach(gasto => {
+            const{nombre,cantidad,id} = gasto;
+            
+            //Crear LI
+            const nuevoGasto= document.createElement('li');
+            nuevoGasto.className = 'list-group-item d-flex justify-content-between align-items-center';
+            nuevoGasto.dataset.id = id; //Asignando el id del objeto al HTML
+            
+
+            //Agregar el HTML del gasto
+            nuevoGasto.innerHTML=`
+                ${nombre} <span class="badge badge-primary badge-pill"> $${cantidad} </span>
+            
+            `
+
+            //Boton para borrar
+            const btnBorrar=document.createElement('button');
+            btnBorrar.classList.add('btn','btn-danger','borrar-gasto');
+            btnBorrar.textContent='Borrar x'
+            nuevoGasto.appendChild(btnBorrar);
+
+
+            //Agregar al HTML
+            gastoListado.appendChild(nuevoGasto);
+        });
     }
 }
 
@@ -49,4 +106,46 @@ function preguntarPresupuesto(){
     presupuesto = new Presupuesto(presupuestoUsuario);
     ui.insetarPrespuesto(presupuesto)
     
+}
+
+function agregarGastos(e){
+    e.preventDefault();
+
+    //Leer los datos del formulario
+    const nombre=document.querySelector('#gasto').value;
+    const cantidad=Number(document.querySelector('#cantidad').value);
+    
+    //Validar
+    if(nombre==='' || cantidad===''){
+        ui.imprimirAlerta('Ambos campos son obligatorios','error');
+        return;
+    }else if(cantidad<=0 || isNaN(cantidad)){
+        ui.imprimirAlerta('Cantidad no valida','error');
+        return;
+    }
+
+    //Generar un objeto con el gasto
+
+    //Esto es lo contrario a distruction
+
+    const gasto={nombre,cantidad,id: Date.now()} //Esta sintaxis une nombre y cantidad a gasto
+    
+    presupuesto.nuevoGasto(gasto);
+
+    //Solamente una vez que hayamos agregado el gasto al arreglo, entonces podemos imprimir la alerta de exito
+    ui.imprimirAlerta('Agregado correctamente');
+
+    //Imprimir los gastos
+    const {gastos} = presupuesto;
+    ui.agregarGastoListado(gastos);
+
+    //Reiniciando el formulario
+    formulario.reset();
+
+}
+
+function limpiarHTML(){
+    while(gastoListado.firstChild){
+        gastoListado.removeChild(gastoListado.firstChild);
+    }
 }
