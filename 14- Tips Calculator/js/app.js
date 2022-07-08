1bebc91afa5fe2207247d260b17a949dc5a4efd7
RicardoPayan <a218206581@unison.mx>
2022-07-08 10:17:20 -0700

    Halfway

--- 14- Tips Calculator/js/app.js
+++ 14- Tips Calculator/js/app.js
@@ -4,9 +4,102 @@ let cliente = {
     pedido:[]
 }
 
+const categorias = {
+    1:'Comida',
+    2: 'Bebidas',
+    3: 'Postres'
+
+}
+
 const btnGuardarCliente = document.querySelector('#guardar-cliente');
 btnGuardarCliente.addEventListener('click',guardarCliente);
 
 function guardarCliente(){
-    console.log('desde la funcion')
+    const mesa = document.querySelector('#mesa').value;
+    const hora = document.querySelector('#hora').value;
+    
+    //Revisar si un campo esta vacio
+    if(mesa === '' || hora === ''){
+        
+        const existeAlerta = document.querySelector('.invalid-feedback');
+
+        if(!existeAlerta){
+            const alerta = document.createElement('div');
+            alerta.classList.add('invalid-feedback','d-block','text-center');
+            alerta.textContent = 'Todos los campos son obligatorios';
+            document.querySelector('.modal-body form').appendChild(alerta);
+
+            //Eliminar alerta
+            setTimeout(() => {
+                alerta.remove();
+            }, 3000);
+        }
+        return;
+    }
+
+    //Asignar datos del formulario a cliente
+    cliente = {...cliente,mesa,hora}
+    
+    //ocultar modal
+    const modalFormulario = document.querySelector('#formulario');
+    const modalBootstrap = bootstrap.Modal.getInstance(modalFormulario);
+    modalBootstrap.hide();
+
+    //Mostrar las secciones
+    mostrarSecciones();
+
+    //Obtener platillos de la API de JSON-Server
+    obtenerPlatillos();
+}    
+
+function mostrarSecciones(){
+    const seccionesOcultas = document.querySelectorAll('.d-none');
+    
+    seccionesOcultas.forEach(seccion => {
+        seccion.classList.remove('d-none')
+    });
+}
+
+function obtenerPlatillos(){
+    const url='http://localhost:4000/platillos';
+
+    fetch(url)
+        .then(respuesta => respuesta.json())
+        .then(resultado => mostrarPlatillos(resultado))
+        .catch(error => console.log(error));    
+}
+
+function mostrarPlatillos(platillos){
+
+    const contenido = document.querySelector('#platillos .contenido');
+
+    
+    platillos.forEach(platillo => {
+        const {id,nombre,precio,categoria} = platillo;
+
+        console.log(categoria)
+
+        const row = document.createElement('DIV');
+        row.classList.add('row','py-3','border-top');
+        
+        const nombrePlatillo = document.createElement('DIV');
+        nombrePlatillo.classList.add('col-md-4');
+        nombrePlatillo.textContent = nombre;
+
+        const precioPlatillo = document.createElement('div');
+        precioPlatillo.classList.add('col-md-3','fw-bold');
+        precioPlatillo.innerHTML = `$${precio}`;
+
+        const categoriaPlatillo = document.createElement('div');
+        categoriaPlatillo.classList.add('col-md-3');
+        categoriaPlatillo.textContent = categorias[categoria]
+        
+        
+
+        row.appendChild(nombrePlatillo);
+        row.appendChild(precioPlatillo);
+        row.appendChild(categoriaPlatillo);
+        
+        contenido.appendChild(row);
+    });
 }
