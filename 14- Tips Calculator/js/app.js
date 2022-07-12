04eadf38af6ef72e9e2715f0598de30ef029e0f8
RicardoPayan <a218206581@unison.mx>
2022-07-12 11:04:47 -0700

    Orders section ready

--- 14- Tips Calculator/js/app.js
+++ 14- Tips Calculator/js/app.js
@@ -147,4 +147,172 @@ function agregarPlatillo(producto){
         cliente.pedido = [...resultado];
     }
 
+    //Limpiar el codigo HTML previo
+    limpiarHTML();
+
+    if(cliente.pedido.length){
+        //Mostrar Resumen
+        actualizarResumen();
+    }else{
+        mensajePedidoVacio();
+    }
+
+}
+
+function actualizarResumen(){
+    const contenido = document.querySelector('#resumen .contenido');
+
+    const resumen = document.createElement('div');
+    resumen.classList.add('col-md-6','card','py-5','px-3','shadow');
+
+    //----Informacion de la mes--------
+    const mesa = document.createElement('p');
+    mesa.textContent = 'Mesa: ';
+    mesa.classList.add('fw-bold');
+
+    const mesaSpan = document.createElement('span');
+    mesaSpan.textContent = cliente.mesa;
+    mesaSpan.classList.add('fw-normal');
+
+    //----Informacion de la hora--------
+    const hora = document.createElement('p');
+    hora.textContent = 'Hora: ';
+    hora.classList.add('fw-bold');
+
+    const horaSpan = document.createElement('span');
+    horaSpan.textContent = cliente.hora;
+    horaSpan.classList.add('fw-normal');
+
+
+    //--Agregar a los elementos padre---
+    mesa.appendChild(mesaSpan);
+    hora.appendChild(horaSpan);
+
+    //---Titulo de la seccion---
+    const heading = document.createElement('h3');
+    heading.textContent = 'Platillos Consumidos';
+    heading.classList.add('my-4','text-center');
+
+    //--Iterar sobre el array de pedidos--
+    const grupo = document.createElement('UL');
+    grupo.classList.add('list-group');
+
+    const {pedido}=cliente;
+    pedido.forEach(articulo => {
+        const {nombre,cantidad,precio,id} = articulo;
+
+        const lista = document.createElement('li');
+        lista.classList.add('list-group-item');
+
+        //Nombre del articulo
+        const nombreEl = document.createElement('h4');
+        nombreEl.classList.add('my-4');
+        nombreEl.textContent = nombre;
+
+        //Cantidad del articulo
+        const cantidadEL = document.createElement('p');
+        cantidadEL.classList.add('fw-bold');
+        cantidadEL.textContent = 'Cantidad: ';
+
+        const cantidadValor = document.createElement('span');
+        cantidadValor.classList.add('fw-normal');
+        cantidadValor.textContent = cantidad;
+
+        cantidadEL.appendChild(cantidadValor);
+
+        //Precio del articulo
+        const precioEl = document.createElement('p');
+        precioEl.classList.add('fw-bold');
+        precioEl.textContent = '$';
+
+        const precioValor = document.createElement('span');
+        precioValor.classList.add('fw-normal');
+        precioValor.textContent = precio;
+
+        precioEl.appendChild(precioValor);
+
+        //Subtotal
+        const subtotalEl = document.createElement('p');
+        subtotalEl.classList.add('fw-bold');
+        subtotalEl.textContent = 'Subtotal: ';
+
+        const subtotalValor = document.createElement('span');
+        subtotalValor.classList.add('fw-normal');
+        subtotalValor.textContent = calcularSubtotal(precio,cantidad);
+
+        subtotalEl.appendChild(subtotalValor);
+
+        //Boton para eliminar
+        const btnEliminar = document.createElement('button');
+        btnEliminar.classList.add('btn','btn-danger');
+        btnEliminar.textContent = 'Eliminar del Pedido';
+
+        //Funcion para eliminar del pedido
+        btnEliminar.onclick = function(){
+            eliminarProducto(id);
+        }
+
+        //Agregar elementos al LI
+        lista.appendChild(nombreEl);
+        lista.appendChild(cantidadEL);
+        lista.appendChild(precioEl);
+        lista.appendChild(subtotalEl);
+        lista.appendChild(btnEliminar);
+    
+        //Agregar lista al grupo principal
+        grupo.appendChild(lista);
+    });
+
+
+    //--Agregar al contenido---
+    resumen.appendChild(mesa);
+    resumen.appendChild(hora); 
+    resumen.appendChild(heading);
+    resumen.appendChild(grupo);
+
+    contenido.appendChild(resumen);
+
+}
+
+function limpiarHTML(){
+    const contenido = document.querySelector('#resumen .contenido');
+
+    while(contenido.firstChild){
+        contenido.removeChild(contenido.firstChild);
+    }
+}
+
+const calcularSubtotal = (precio,cantidad) => `$${precio*cantidad}`;
+
+function eliminarProducto(id){
+    const {pedido} = cliente;
+
+    //Eliminar elementos cuando la cantidad es cero
+    const resultado = pedido.filter(articulo => articulo.id !== id);
+    cliente.pedido = [...resultado];
+
+    limpiarHTML();
+    if(cliente.pedido.length){
+        //Mostrar Resumen
+        actualizarResumen();
+    }else{
+        mensajePedidoVacio();
+    }
+
+    //El producto se elimino por lo tanto regresamos la cantidad a 0 en el formulario
+    const productoEliminado = `#producto-${id}`;
+    const inputEliminado = document.querySelector(productoEliminado);
+    inputEliminado.value = 0;
+    
+    
+}
+
+function mensajePedidoVacio(){
+    const contenido = document.querySelector('#resumen .contenido');
+
+    const texto = document.createElement('p');
+    texto.classList.add('text-center');
+    texto.textContent = 'Añade los elementos del pedido';
+
+    contenido.appendChild(texto);
 }
